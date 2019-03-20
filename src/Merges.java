6160d08665a90b480968f63834cffbba5946c257
Derek Riley <derek.jabari@gmail.com>
2019-03-20 11:26:21 -0700

    Commit Conflict

--- src/Merges.java
+++ src/Merges.java
@@ -3,6 +3,8 @@ public class Merges {
 	
 	public static void main(String args[]) {
 		System.out.println("Hello from the master branch");
+		
+		System.out.println("Hello from the new-feature branch");
 	}
 	
 }
